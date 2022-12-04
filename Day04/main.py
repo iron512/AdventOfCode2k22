8b6280dac82c8b7929dd5da56902a5fe63bbd9c7
Ivan Martini <ivan.martini@wuerth.it>
2022-12-04 23:59:58 +0100

    chore(Day4): refactored set handling for Day4

--- Day04/main.py
+++ Day04/main.py
@@ -3,26 +3,24 @@ def main(test):
 	if not test:
 		rows = open("input.txt").read().split("\n")
 
+	inclusions = 0
 	overlap = 0
-	overlap_partial = 0
 	for pair in rows:
-		elf1, elf2 = pair.split(",")
-		elf1_s, elf1_e = [int(val) for val in elf1.split("-")]
-		elf2_s, elf2_e = [int(val) for val in elf2.split("-")]
-		elf1_range = set(range(elf1_s, elf1_e + 1))
-		elf2_range = set(range(elf2_s, elf2_e + 1))
+		[elf1_start, elf1_end], [elf2_start, elf2_end] = [interval.split("-") for interval in pair.split(",")]
+		elf1_range = set(range(int(elf1_start), int(elf1_end) + 1))
+		elf2_range = set(range(int(elf2_start), int(elf2_end) + 1))
 
 		intersect = elf1_range.intersection(elf2_range)
 
-		if len(intersect) == min(len(elf1_range),len(elf2_range)):
+		if len(intersect) == min(len(elf1_range), len(elf2_range)):
+			inclusions += 1
+
+		if len(intersect) != 0:
 			overlap += 1
 
-		if (len(intersect)) != 0:
-			overlap_partial += 1
-
-	print("The overlapping sets are:", overlap)
-	print("The partially overlapping sets are:", overlap_partial)
+	print("The overlapping sets are:", inclusions)
+	print("The partially overlapping sets are:", overlap)
 
 
 if __name__ == '__main__':
-	main(False)
+	main(False	)
