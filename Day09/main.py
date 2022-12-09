aa954e7b8d88cad1912e452f38aa36a8be729038
Ivan Martini <ivan.martini@wuerth.it>
2022-12-09 12:07:38 +0100

    feat(Day9): refactored day9 part1 to generalize also for part 2. Less a pain than expected

--- Day09/main.py
+++ Day09/main.py
@@ -1,60 +1,71 @@
-# COSTANTS - useful mapping
+# CONSTANTS - useful mapping
 direction_map = 'UDLR'
 deltax = [0, 0, -1, 1]
 deltay = [1, -1, 0, 0]
 
 
-class Rope:
+class Knot:
 	def __init__(self):
-		self.hx = 0
-		self.hy = 0
-		self.tx = 0
-		self.ty = 0
-		self.h_visited = [(0, 0)]
-		self.t_visited = [(0, 0)]
+		self.x = 0
+		self.y = 0
+		self.visited = []
 
-	def visit_head(self):
-		self.h_visited.append((self.hx, self.hy))
-
-	def visit_tail(self):
-		self.t_visited.append((self.tx, self.ty))
-
-	def visited_tails(self):
-		return len(set(self.t_visited))
-
-	def move_head(self, direction):
+	def move(self, direction):
 		dir_value = direction_map.index(direction)
-		self.hx += deltax[dir_value]
-		self.hy += deltay[dir_value]
-		self.visit_head()
-		self.follow()
+		self.x += deltax[dir_value]
+		self.y += deltay[dir_value]
+		self.visit()
 
-	def follow(self):
-		dx = self.hx-self.tx
-		dy = self.hy-self.ty
+	def follow(self, prior):
+		dx = prior.x-self.x
+		dy = prior.y-self.y
 		if max(abs(dx), abs(dy)) > 1:
 			# the rope must move
-			self.tx += int(dx/abs(dx)) if dx != 0 else 0
-			self.ty += int(dy/abs(dy)) if dy != 0 else 0
-			self.visit_tail()
+			self.x += int(dx/abs(dx)) if dx != 0 else 0
+			self.y += int(dy/abs(dy)) if dy != 0 else 0
+		self.visit()
+
+	def visit(self):
+		self.visited.append((self.x, self.y))
+
+
+class Rope:
+	def __init__(self, count):
+		self.knots = [Knot() for i in range(count)]
+
+	def visited_tails(self):
+		return len(set(self.knots[-1].visited))
+
+	def move(self, direction):
+		prec = None
+		for knot in self.knots:
+			if prec is None:
+				knot.move(direction)
+			else:
+				knot.follow(prec)
+			prec = knot
 
 
 def main(test):
 	rows = open("input_easy.txt").read().split("\n")
+	rows = open("input_hard.txt").read().split("\n")
 	if not test:
 		rows = open("input.txt").read().split("\n")
 
-	rope = Rope()
+	ropefirst = Rope(2)
+	ropesecond = Rope(10)
 	for row in rows:
 		direct, times = row.split(" ")
 		times = int(times)
 
 		while times:
-			rope.move_head(direct)
+			ropefirst.move(direct)
+			ropesecond.move(direct)
 			times -= 1
 
 		# print(rope.hx, rope.hy, "-", rope.tx, rope.ty)
-	print("The number of spaces visited by tail is:", rope.visited_tails())
+	print("The number of spaces visited by the 2 knot rope's tail is:", ropefirst.visited_tails())
+	print("The number of spaces visited by the 10 knot rope's tail is:", ropesecond.visited_tails())
 
 
 if __name__ == '__main__':
